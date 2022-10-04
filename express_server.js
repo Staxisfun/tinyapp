719cfbef2d0ba36bb4cf86f33d62108d1a35f815
Matt Wilson <mwilson82@hotmail.com>
2022-10-04 02:34:24 +0000

    implemented a random number generator function and completed URL shortening part 1

--- express_server.js
+++ express_server.js
@@ -13,6 +13,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -30,11 +33,29 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+
+  let random = (Math.random().toString(36).slice(6, 12))
+
+  return random
+}
