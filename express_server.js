c6df397ef373c615a037f285b6f60f580553c1f9
Matt Wilson <mwilson82@hotmail.com>
2022-10-05 04:46:34 +0000

    implemented login and logout functionality to tinyApp

--- express_server.js
+++ express_server.js
@@ -15,6 +15,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser())
 
 
 app.get("/", (req, res) => {
@@ -36,20 +37,21 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 });
 
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies['username']}
+  res.render("urls_new", templateVars);
 });
 
 
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username'] };
   res.render("urls_show", templateVars);
 });
 
@@ -93,11 +95,17 @@ res.redirect("/urls")
 
 app.post("/login", (req, res) => {
   
-  const username = req.body
+  const username = req.body.username
 
-
-res.cookie('usernameCookie', username)
+console.log(username)
+res.cookie('username', username)
 res.redirect("/urls")
+})
+
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+ res.redirect("/urls") 
 })
 
 
